c2603cc838d8893ffa313ce1f6b4cc84394b659b
Joao Batista <batista.freebsd@gmail.com>
2019-12-29 22:11:09 -0300

    New update to main file..

--- 2.C++Beginner/1-4Console/main.cpp
+++ 2.C++Beginner/1-4Console/main.cpp
@@ -6,5 +6,15 @@ int main()
 {
     cout << "Hello World!" << endl ;
     cout << "How are you doing" << endl;
+    
+    //another example for std
+    cout << "Another examples for <std>" << endl;
+     
+    //For errors
+    cerr << "There was an error..!" << endl;
+    
+    //For logs
+    clog << "This is a log message..!" << endl;
+    
     return 0;
 }
